fa8296786ee9c3656338448338511e1b7e78ce63
kmkacpermajor <kmkacpermajor@gmail.com>
2023-12-12 10:05:21 +0100

    ipaddressvalidator

--- src/authverifier/authverifier.cpp
+++ src/authverifier/authverifier.cpp
@@ -1,47 +1,42 @@
-#include <string>
-#include <deque>
-
 #include "authverifier.hpp"
-#include "../tcpconnection/tcpconnection.hpp"
-#include "../sqliteconnector/sqliteconnector.hpp"
-#include "../sqlitequery/sqlitequery.hpp"
+
+#include <deque>
+#include <string>
+
 #include "../misc/stringTrim.hpp"
 #include "../misc/types.hpp"
-#include "../ipaddressvalidator/ipaddressvalidator.hpp"
+#include "../sqliteconnector/sqliteconnector.hpp"
+#include "../sqlitequery/sqlitequery.hpp"
+#include "../tcpconnection/tcpconnection.hpp"
 
-AuthVerifierError::AuthVerifierError(const std::string message){
-    this->message = message;
+AuthVerifierError::AuthVerifierError(const std::string message) {
+  this->message = message;
 }
 
-std::string AuthVerifierError::what() {
-  return this->message;
-}
+std::string AuthVerifierError::what() { return this->message; }
 
-AuthVerifier::AuthVerifier(TCPConnection &conn, SQLiteConnector &dbC) : connection(conn), dbConnector(dbC)
-{
-}
+AuthVerifier::AuthVerifier(TCPConnection &conn, SQLiteConnector &dbC)
+    : connection(conn), dbConnector(dbC) {}
 
-bool AuthVerifier::checkIfLoggedIn(){
-    if(connection.getCurrentUser() == ""){
-        return false;
-    }
-    return true;
+bool AuthVerifier::checkIfLoggedIn() {
+  if (connection.getCurrentUser() == "") {
+    return false;
+  }
+  return true;
 }
 
 void AuthVerifier::verifyCommand(std::string command, paramDeque params) {
-    if(isStringInVector(commandsLoggedIn, command) && !checkIfLoggedIn()){
-        throw AuthVerifierError("This command can be used only when logged in");
-    }
+  if (isStringInVector(commandsLoggedIn, command) && !checkIfLoggedIn()) {
+    throw AuthVerifierError("This command can be used only when logged in");
+  }
 
-    if(isStringInVector(commandsNotLoggedIn, command) && checkIfLoggedIn()){
-        throw AuthVerifierError("This command can be used only when logged off");
-    }
+  if (isStringInVector(commandsNotLoggedIn, command) && checkIfLoggedIn()) {
+    throw AuthVerifierError("This command can be used only when logged off");
+  }
 
-    if(command == "shutdown"){
-        verifyShutdown(params.at(0));
-    }
+  if (command == "shutdown") {
+    verifyShutdown(params.at(0));
+  }
 }
 
-void AuthVerifier::verifyShutdown(std::string IP){
-    
-}
+void AuthVerifier::verifyShutdown(std::string IP) {}
